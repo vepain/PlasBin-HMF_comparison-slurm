856336282cc8025e6718e7e6b2000dfb7b59e9bd
vepain <victorepain@disroot.org>
2026-07-10 17:36:03 +0200

    feat: :sparkles: Add complete list of samples

--- init.sh
+++ init.sh
@@ -15,16 +15,23 @@ bench_root_dir=$1
 
 umask 007
 
+mkdir -p "$bench_root_dir"
+bench_root_dir=$(realpath "$bench_root_dir") # must create the directory before knowing its absolute path
 cp -R src/* "$bench_root_dir"
 
 # ---------------------------------------------------------------------------- #
-# In every shell script, replace the TODO:BENCH_ROOT_DIR token
+# In every shell script, replace the /media/profchep/linux_work/PROJETS/Plasmid/Binning/PlasBin-HMF/PlasBin-HMF_comparison-slurm token
 # ---------------------------------------------------------------------------- #
 find "$bench_root_dir" -type f -name '*.sh' -exec \
-    sed -i "s|TODO:BENCH_ROOT_DIR|$bench_root_dir|g" {} +
+    sed -i "s|/media/profchep/linux_work/PROJETS/Plasmid/Binning/PlasBin-HMF/PlasBin-HMF_comparison-slurm|$bench_root_dir|g" {} +
 
 # ---------------------------------------------------------------------------- #
 # Make all scripts executable
 # ---------------------------------------------------------------------------- #
 find "$bench_root_dir" -type f -name '*.sh' -exec \
     chmod +x {} +
+
+# ---------------------------------------------------------------------------- #
+#                            Create A Data Directory                           #
+# ---------------------------------------------------------------------------- #
+mkdir -p "$bench_root_dir/data"
