88743563d10e03f1e66c81a6d90e7f3822043c5d
vepain <victorepain@disroot.org>
2026-07-13 15:49:53 +0200

    refactor: :recycle: Use config script

--- scripts/fir_envs/plaseval-gdv.sh
+++ scripts/fir_envs/plaseval-gdv.sh
@@ -2,28 +2,31 @@
 # ---------------------------------------------------------------------------- #
 # Installation PlasEval (Gianluca Della Vedova's fork)
 # ---------------------------------------------------------------------------- #
-benchmark_root_dir=$1
+BENCH_ROOT_DIR=$1
+# shellcheck source=../../src/scripts/config.sh
+source "$BENCH_ROOT_DIR/scripts/config.sh" "$BENCH_ROOT_DIR"
+
+running_dir=$(pwd)
 
 umask 007
 
+# ---------------------------------------------------------------------------- #
+# Build the apptainer image in scratch
+# ---------------------------------------------------------------------------- #
 cd "/scratch/$USER" || exit 1
 git clone https://github.com/gdv/PlasEval.git PlasEval-GDV
 cd PlasEval-GDV || exit 1
-
-# Build the apptainer image on `fir` cluster (on scratch):
 
 module load apptainer
 APPTAINER_BIND=" "
 export APPTAINER_BIND
 apptainer build PlasEval-GDV.sif PlasEval.def
 
-# Test the apptainer image:
-
-apptainer run -C PlasEval-GDV.sif --help
-
-# Move it to the `$benchmark_root_dir/envs` directory:
-
-mv PlasEval-GDV.sif "$benchmark_root_dir/envs"
+#
+# Finish
+#
+mv PlasEval-GDV.sif "$BENCH_ENVS_DIR"
 cd ..
 # Remove the git repository
 rm -rf PlasEval-GDV
+cd "$running_dir" || exit 1
