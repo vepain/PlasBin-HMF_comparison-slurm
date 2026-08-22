6dfedf2222445b5d730a00385befbafc742642b0
vepain <victorepain@disroot.org>
2026-08-22 14:14:01 +0200

    fix: :bug: APPTAINER binding now bench_root_dir

--- src/envs/plaseval-gdv.sh
+++ src/envs/plaseval-gdv.sh
@@ -8,4 +8,4 @@ module load apptainer
 
 APPTAINER_IMG="$BENCH_ENVS_DIR/PlasEval-GDV.sif"
 # See https://docs.alliancecan.ca/wiki/Apptainer#Bind_mounts
-export APPTAINER_BINDPATH="/project,/scratch"
+export APPTAINER_BINDPATH="/project,/scratch,$BENCH_ROOT_DIR"
